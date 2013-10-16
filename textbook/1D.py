7f3368c429b7f12db3fb76e21654f77b29191174
Joel Thibault <thibault@broadinstitute.org>
2013-10-15 21:47:45 -0400

    More efficient: only goes through sequence once

--- textbook/1D.py
+++ textbook/1D.py
@@ -21,19 +21,29 @@ import inout 	# my module for handling Rosalind's file I/O
 sequence = inout.infilelines[0].strip()
 k, L, t = map(int, inout.infilelines[1].strip().split())
 
-kmers = []
-for window_start in range(len(sequence) - L + 1):
-	window_kmers = {}
-	for offset in range(L - k + 1):
-		kmer = sequence[window_start+offset:window_start+offset+k]
-		if kmer in window_kmers:
-			count = window_kmers[kmer] + 1
-		else:
-			count = 1		
-		window_kmers[kmer] = count
-	
-		if count >= t:
-			kmers.append(kmer)
+kmers_to_check = []
+kmer_locations = {}
+for idx in range(len(sequence) - k + 1):
+	kmer = sequence[idx:idx+k]
+	if kmer in kmer_locations:
+		kmer_locations[kmer].append(idx)
+	else:
+		kmer_locations[kmer] = [idx]
+		
+	if len(kmer_locations[kmer]) >= t:
+		kmers_to_check.append(kmer)
 			
-resultset = " ".join(set(kmers))
+kmers_to_keep = []
+for kmer in set(kmers_to_check):
+	locations = kmer_locations[kmer]
+
+	# once we've passed location[-t+1] then we can't have t locations in the window
+	for first_loc_index, first_loc in enumerate(locations[:-t+1]):
+		last_in_window = first_loc + L - k	
+		locs_in_window = filter(lambda loc: loc <= last_in_window, locations[first_loc_index:])	 			
+		if len(locs_in_window) >= t:
+			kmers_to_keep.append(kmer)
+			break
+		
+resultset = " ".join(set(kmers_to_keep))
 inout.output(resultset)
